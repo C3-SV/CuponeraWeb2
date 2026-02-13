01b3b3a33bc860cfa7727c9a65aea36994fd9b4c
ChrisM2309 <chris.marroquin2309@gmail.com>
2026-02-13 11:08:09 -0600

    Estrutura React y de Archivos completada

---
+++ LaCuponera/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='p-6 text-7xl'>La Cuponera</h1>
+    </>
+  )
+}
+
+export default App
