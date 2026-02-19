fcbe7e294def91823da11656184dceaa39d5ea26
itsRoberOOF <83309227+itsRoberOOF@users.noreply.github.com>
2026-02-18 21:41:59 -0600

    arreglo de boton de filtros

--- src/components/offers/FiltersLayout.jsx
+++ src/components/offers/FiltersLayout.jsx
@@ -74,7 +74,7 @@ export const FiltersLayout = ({
                     <button
                         type="button"
                         onClick={onApply}
-                        className="flex w-full items-center justify-center rounded-lg border border-transparent bg-primary px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-hover focus:outline-none focus:ring-2 focus:primary focus:ring-offset-2 transition-all active:scale-95"
+                        className="flex w-full items-center justify-center rounded-lg border border-transparent bg-primary px-4 py-2.5 text-sm font-medium text-white shadow-sm hover:bg-primary-hover focus:outline-none focus:ring-2 focus:primary focus:ring-offset-2 transition-all active:scale-95 font-heading tracking-wide"
                     >
                         Aplicar Filtros
                     </button>
