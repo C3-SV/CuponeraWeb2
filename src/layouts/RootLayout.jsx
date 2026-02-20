37317d1c8817aaa482da2e5fb86aef506c830c01
fiorexyz <angiefiorellaguzman2006@gmail.com>
2026-02-20 11:07:22 -0600

    perfil - primer commit

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
 
 const linkDesktop = ({ isActive }) =>
     `text-sm transition ${
@@ -17,6 +18,7 @@ const linkMobile = ({ isActive }) =>
 
 export default function RootLayout() {
     const [mobileOpen, setMobileOpen] = useState(false);
+    const session = useAuthStore((s) => s.session);
 
     // Cerrar con ESC
     useEffect(() => {
@@ -82,9 +84,15 @@ export default function RootLayout() {
                                 <path d="M6 2a1 1 0 0 1 .993 .883l.007 .117v1.068l13.071 .935a1 1 0 0 1 .929 1.024l-.01 .114l-1 7a1 1 0 0 1 -.877 .853l-.113 .006h-12v2h10a3 3 0 1 1 -2.995 3.176l-.005 -.176l.005 -.176c.017 -.288 .074 -.564 .166 -.824h-5.342a3 3 0 1 1 -5.824 1.176l-.005 -.176l.005 -.176a3.002 3.002 0 0 1 1.995 -2.654v-12.17h-1a1 1 0 0 1 -.993 -.883l-.007 -.117a1 1 0 0 1 .883 -.993l.117 -.007h2zm0 16a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm11 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z" />
                             </svg>
                         </NavLink>
-                        <NavLink to="/login" className={linkDesktop}>
-                            Iniciar sesión
-                        </NavLink>
+                        {session ? (
+                            <NavLink to="/profile" className={linkDesktop}>
+                                Mi perfil
+                            </NavLink>
+                        ) : (
+                            <NavLink to="/login" className={linkDesktop}>
+                                Iniciar sesión
+                            </NavLink>
+                        )}
                     </div>
                 </nav>
             </header>
@@ -195,13 +203,23 @@ export default function RootLayout() {
                                         Mi carrito
                                     </NavLink>
 
-                                    <NavLink
-                                        to="/login"
-                                        className={linkMobile}
-                                        onClick={() => setMobileOpen(false)}
-                                    >
-                                        Iniciar sesión
-                                    </NavLink>
+                                    {session ? (
+                                        <NavLink
+                                            to="/profile"
+                                            className={linkMobile}
+                                            onClick={() => setMobileOpen(false)}
+                                        >
+                                            Mi perfil
+                                        </NavLink>
+                                    ) : (
+                                        <NavLink
+                                            to="/login"
+                                            className={linkMobile}
+                                            onClick={() => setMobileOpen(false)}
+                                        >
+                                            Iniciar sesión
+                                        </NavLink>
+                                    )}
                                 </nav>
                             </div>
                         </div>
